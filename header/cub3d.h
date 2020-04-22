93daeadfeb422da5384b2748dacfb7e4e9328d5d
mabriand-42 <briandmarie@outlook.fr>
2020-04-22 16:33:38 +0200

    modified

--- header/cub3d.h
+++ header/cub3d.h
@@ -70,14 +70,14 @@ typedef struct	s_check
 	t_bool	Ceiling_color;
 }				t_check;
 
-typedef struct	s_configuration
+typedef struct	s_config
 {
 	t_resolution	R;
 	t_color			F;
 	t_color			C;
 	t_textures		T;
 	t_check			check;
-}				t_configuration;
+}				t_config;
 
 typedef enum	s_cardinal
 {
@@ -114,20 +114,25 @@ void			ft_skip_spaces(char *str, size_t *pos);
 int				ft_find_char(char c, char *str);
 int				ft_isvalidchar(char c);
 char			*ft_join_bis(char *s1, char *s2);
+char			*ft_str_null_bis(char *s1, char *s2);
 
 /*
 ** Config
 */
 
 int				ft_check_id(char c1, char c2);
-int				ft_find_id(char *line, char *identifier, size_t *pos);
+int				ft_find_id(char *line, char *id, size_t *pos);
+int				ft_id_s(size_t *pos, char *line, char *id);
+int				ft_id_rfc(size_t *pos, char *line, char *id);
+int				ft_id_nwe(size_t *pos, char *line, char *id);
 int				ft_check_config(t_check check);
+void			ft_update_player(char c, t_map *m);
 
 /*
 ** Init
 */
 
-void			ft_init_config(t_configuration *config);
+void			ft_init_config(t_config *config);
 void			ft_init_check(t_check *check);
 void			ft_init_map(t_map *map);
 
@@ -159,20 +164,21 @@ int				ft_t(char *line, size_t *pos, char **path, t_bool *check);
 */
 
 int				ft_closed_map(char *line, size_t *pos);
-int				ft_check_isolated_line(char *line);
 int				ft_cmp_char(char c1, char c2, t_map *map);
 int				ft_cmp_prev_next(char c1, char *line, size_t index);
 int				ft_duo_line(char *prev, char *next, t_map *map);
+int				ft_duo_line_bis(char *prev, char *next, size_t *ptr_i, size_t *ptr_j);
 
 void			ft_join_for_map(t_map *map, char *next, int *ret);
-int				ft_lines(char *line, char *next, t_map *map, t_configuration *config);
+int				ft_lines(char *line, char *next, t_map *map, t_config *config);
 void			ft_transform_map(t_map *map);
 
 /*
 ** Parsing
 */
 
-int				ft_elem_parsing(char *line, t_configuration *config);
-int				ft_orient_gnl(int ret_gnl, t_map *map, t_configuration *config, char *line, char *next);
+int				ft_elem_parsing(char *line, t_config *config);
+int				ft_orient_gnl(int ret_gnl, t_map *map, t_config *config, char *line, char *next);
+int				ft_get_config(char *id, char *line, size_t *pos, t_config *c);
 
 #endif
